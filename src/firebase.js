19593562c4b5913f67c6fd44794d92856aaf1adc
Anushasetti <anushasetti42@gmail.com>
2021-10-05 20:00:07 +0530

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD3P-qr7Mrlg74BQymT7bykV2HYwbU8Gk8",
+  authDomain: "amz-clone-medium.firebaseapp.com",
+  databaseURL: "https://amz-clone-medium.firebaseio.com",
+  projectId: "amz-clone-medium",
+  storageBucket: "amz-clone-medium.appspot.com",
+  messagingSenderId: "769450879220",
+  appId: "1:769450879220:web:cbff61e0ee041531e456a1",
+  measurementId: "G-CZDBWXMWF7",
+};
+
