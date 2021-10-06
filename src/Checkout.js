a74f497d868d20a641d74ce18dc739f49b1e9672
Anushasetti <anushasetti42@gmail.com>
2021-10-06 22:10:38 +0530

    deploy app to github pages

--- src/Checkout.js
+++ src/Checkout.js
@@ -2,15 +2,15 @@ import React from 'react'
 import './Checkout.css';
 import Subtotal from './Subtotal';
 import CheckoutProduct from './CheckoutProduct';
-import { getBasketTotal } from './reducer';
+//import { getBasketTotal } from './reducer';
 import { useStateValue } from './StateProvider';
 
 function Checkout() {
-    const [{basket},dispatch]=useStateValue();
+    const [{basket}]=useStateValue();
     return (
         <div className="checkout">
             <div className="checkout_left">
-                <img className="checkout_ad" src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"/>
+                <img className="checkout_ad" src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg" alt="checking"/>
                 <div>
                     <h2 classNmae="checkout_title">My Shopping Basket</h2>
                     {basket.map(item =>(
--- src/Login.js
+++ src/Login.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './Login.css'
-import { Link, useHistory } from "react-router-dom";
-import { auth } from "./firebase";
+import { Link} from "react-router-dom";
+//import { auth } from "./firebase";
 
 function Login() {
     
@@ -13,7 +13,8 @@ function Login() {
             <Link to='/'>
                 <img
                     className="login__logo"
-                    src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png' 
+                    src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png'
+                    alt="loading" 
                 />
             </Link>
 
